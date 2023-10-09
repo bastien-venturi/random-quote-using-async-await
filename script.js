51577c5e24f693c62d408f63ca76c7b4d7878e9a
Venturi <bastien.aristide.venturi@gmail.com>
2023-10-09 15:23:20 +0200

    end

--- script.js
+++ script.js
@@ -1,60 +1,7 @@
-// let section1 = document.querySelector(".section1");
-// let main = document.querySelector("main");
-// let btn1 = document.createElement("button");
-// btn1.textContent = 'Push me!';
-// main.appendChild(btn1);
-// main.appendChild(section1);
-
-
-// btn1.addEventListener("click", function () {
-
-//     while (section1.firstChild) {
-//         section1.removeChild(section1.firstChild);
-//     }
-
-//     const fetchName = () => fetch("https://thatsthespir.it/api");
-//     fetchName()
-//         .then((response) => response.json())
-//         .then((json) => {        
-//                 console.log(json);
-
-//             	let author = document.createElement('h1')
-//                 let photo = document.createElement('img')
-//                 let quote = document.createElement('p')
-
-//                 author.textContent = json.author;
-//                 photo.src = json.photo;
-//                 photo.alt = json.author;
-//                 quote.textContent = json.quote;
-
-//                 section1.appendChild(author)
-//                 section1.appendChild(photo)
-//                 section1.appendChild(quote)
-                
-//                 function resolveAfter2Seconds() {
-//                     return new Promise((resolve) => {
-//                       setTimeout(() => {
-//                         resolve('resolved');
-//                       }, 2000);
-//                     });
-//                   }
-                  
-//                   async function asyncCall() {
-//                     console.log('calling');
-//                     const result = await resolveAfter2Seconds();
-//                     console.log(result);
-//                   }
-                  
-//                   asyncCall();
-                  
-//         })
-//         .catch((error) => {
-//             console.log("There was an error!", error);
-//         });
-//     });
 let section1 = document.querySelector(".section1");
 let main = document.querySelector("main");
-let btn1 = document.createElement("button");
+let btn1 = document.createElement("button")
+
 btn1.textContent = 'Push me!';
 main.appendChild(btn1);
 main.appendChild(section1);
@@ -89,20 +36,18 @@ btn1.addEventListener("click", async function () {
         section1.appendChild(photo)
         section1.appendChild(quote)
 
-        async function resolveAfter2Seconds() {
+        async function resolveAfter1Seconds() {
             return new Promise((resolve) => {
                 setTimeout(() => {
                     resolve('resolved');
-                }, 2000);
+                }, 500);
             });
         }
         
-        await resolveAfter2Seconds();
+        await resolveAfter1Seconds();
 
     } 
     catch (error) {
-        console.error("There was an error:", error);
-        // Affiche un message d'erreur à l'utilisateur
         const errormessage = document.createElement('p');
         errormessage.textContent = "Please try again later.";
         section1.appendChild(errormessage);
@@ -111,3 +56,4 @@ btn1.addEventListener("click", async function () {
         btn1.disabled = false;
     }
 });
+btn1.click();
